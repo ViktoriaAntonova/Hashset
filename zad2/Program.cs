d3139e1f7c4df08ee8cdd124cb04bb85f6987f42
MEGA <MEGA@gmail.com>
2025-04-07 18:50:14 +0300

    zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("Maria");
+            set.Add("Ivan");
+            set.Add("Georgi");
+            if(set.Contains("Georgi"))
+            {
+                Console.WriteLine("Imeto sustestvuva");
+            }
+            else
+            {
+                Console.WriteLine("Nqma go");
+            }
+        }
+    }
+}
