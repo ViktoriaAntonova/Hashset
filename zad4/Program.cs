e3b8dad182b6ec7ef209b8ea45ef61979eba5c8f
MEGA <MEGA@gmail.com>
2025-04-07 18:54:47 +0300

    zad4

---
+++ zad4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int> { 1, 2, 3, 4 };
+            List<int> list2 = new List<int> { 5, 6, 7, 3 };
+            HashSet<int> set1 = new HashSet<int>(list1);
+            HashSet<int> set2 = new HashSet<int>(list2);
+            foreach (var item in set1)
+            {
+                if (set2.Contains(item))
+                {
+                    Console.WriteLine($"Obsht element: {item}");
+                    return; 
+                }
+            }
+            Console.WriteLine("Nqma obsht");
+        }
+    }
+}
