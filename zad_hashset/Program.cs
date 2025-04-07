b64a2193692fd5f76390350e6a72eedcc9e37ce7
MEGA <MEGA@gmail.com>
2025-04-07 18:42:35 +0300

    Add project files.

---
+++ zad_hashset/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_hashset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+            set.Add(3);
+            set.Add(4);
+            set.Add(4);
+            set.Add(2);
+            set.Add(3);
+            set.Add(3);
+            set.Add(4);
+            set.Add(2);
+            foreach (var A in set)
+            {
+                Console.WriteLine(A);
+            }
+        }
+    }
+}
