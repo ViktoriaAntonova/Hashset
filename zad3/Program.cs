ac5841a79c773ccdc3401af90629efc8ba99aa8d
MEGA <MEGA@gmail.com>
2025-04-07 18:57:10 +0300

    zad

---
+++ zad3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace zad3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "this is a test. This is only a test.";
+            string[] words = Regex.Split(input.ToLower(), @"\W+");
+            HashSet<string> uniqueWords = new HashSet<string>(words);
+            Console.WriteLine(uniqueWords.Count);
+        }
+    }
+}
